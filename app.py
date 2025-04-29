2bad19e0a93ff9d233fe00bbae1f3e149e1f0007
Tshepiso28 <tshepisostudent@gmail.com>
2025-04-29 10:29:44 +0200

    changes the database name

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def get_db_connection():
     return psycopg2.connect(
         host=os.environ.get('DB_HOST', 'localhost'),
         database=os.environ.get('DB_NAME', 'rentdb'),
-        user=os.environ.get('DB_USER', 'postgres'),
+        user=os.environ.get('DB_USER', 'damacm179'),
         password=os.environ.get('DB_PASSWORD', '1017'),
         cursor_factory=extras.DictCursor
     )
